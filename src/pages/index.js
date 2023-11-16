d83ffa413a4eb0a970607b193e81bd7d6b028de0
Umakanta Meher <100068101+umakantameher117@users.noreply.github.com>
2023-11-16 11:32:55 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,7 +20,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Video Wiki Tutorial
+            VideoWiki Tutorial
           </Link>
         </div>
       </div>
